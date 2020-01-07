70a6c056065240aeb9605b1100e881aa5a008985
nakha <ksnkhm@gmail.com>
2020-01-07 08:24:36 +0400

    control settings completed

--- menu.py
+++ menu.py
@@ -57,6 +57,7 @@ def load_image(name, color_key=None):
 # Вместо game потом надо добавить select_character
 class MainMenu:
     def __init__(self):
+        self.paragraph = 0
         self.points = [(30, 100, 'Подключиться к серверу', (250, 250, 30), (250, 30, 250), 0),
                        (30, 200, 'Создать сервер', (250, 250, 30), (250, 30, 250), 1),
                        (30, 300, 'Настройки', (250, 250, 30), (250, 30, 250), 2),
@@ -71,45 +72,44 @@ class MainMenu:
             else:
                 surface.blit(font_menu.render(i[2], 2, i[3]), (i[0], i[1]))
 
-    def clicked(self, paragraph):
-        if paragraph == 0:
+    def clicked(self, paragraph_number):
+        if paragraph_number == 0:
             # В будующем надо будет вызывать меню подключения к серверу
             return 'connect to server'
-        elif paragraph == 1:
+        elif paragraph_number == 1:
             # В будующем надо будет вызывать меню выбора персонажа место начала игры
             return 'fight'
-        elif paragraph == 2:
+        elif paragraph_number == 2:
             return 'settings'
-        elif paragraph == 3:
+        elif paragraph_number == 3:
             sys.exit()
 
     def run(self):
         running = True
-        paragraph = 0  # номер кнопки
         font = pygame.font.SysFont('serif', 30)
         while running:
             mouse = pygame.mouse.get_pos()
             for i in self.points:
                 # меняем подсвечиваемую кнопку, при наведении на неё мышкой
                 if mouse[0] > i[0] and mouse[0] < i[0] + 155 and mouse[1] > i[1] and mouse[1] < i[1] + 50:
-                    paragraph = i[5]
-            self.drawing(screen, font, paragraph)
+                    self.paragraph = i[5]
+            self.drawing(screen, font, self.paragraph)
             for i in pygame.event.get():
                 if i.type == pygame.QUIT:
                     sys.exit()
                 elif i.type == pygame.KEYDOWN:
                     # меняем подсвечиваемую кнопку кнопками верх/вниз, если это возможно
                     if i.key == pygame.K_UP:
-                        paragraph = (paragraph - 1) % 4
+                        self.paragraph = (self.paragraph - 1) % 4
                     if i.key == pygame.K_DOWN:
-                        paragraph = (paragraph + 1) % 4
+                        self.paragraph = (self.paragraph + 1) % 4
                     # вызываем действия от подсвечиваемой кнопки при нажатии на enter
                     if i.key == 13:
-                        called_menu = self.clicked(paragraph)
+                        called_menu = self.clicked(self.paragraph)
                         running = False
                 # вызываем действия от подсвечиваемой кнопки при нажатии на левую кнопку мыши/enter
                 if i.type == pygame.MOUSEBUTTONDOWN and i.button == 1:
-                    called_menu = self.clicked(paragraph)
+                    called_menu = self.clicked(self.paragraph)
                     running = False
             screen.blit(screen, (0, 0))
             pygame.display.flip()
@@ -117,109 +117,175 @@ class MainMenu:
         return called_menu
 
 
-control = {'ПРЫЖОК': 'W',
-           'ПРИСЕСТЬ': 'S',
+keyboard = {'113': 'Q',
+            '119': 'W',
+            '101': 'E',
+            '114': 'R',
+            '116': 'T',
+            '121': 'Y',
+            '117': 'U',
+            '105': 'I',
+            '111': 'O',
+            '112': 'P',
+            '97': 'A',
+            '115': 'S',
+            '100': 'D',
+            '102': 'F',
+            '103': 'G',
+            '104': 'H',
+            '106': 'J',
+            '107': 'K',
+            '108': 'L',
+            '122': 'Z',
+            '120': 'X',
+            '99': 'C',
+            '118': 'V',
+            '98': 'B',
+            '110': 'N',
+            '109': 'M',
+            '49': '1',
+            '50': '2',
+            '51': '3',
+            '52': '4',
+            '53': '5',
+            '54': '6',
+            '55': '7',
+            '56': '8',
+            '57': '9',
+            '48': '0',
+            '301': 'CAPS LOCK',
+            '304': 'SHIFT_L',
+            '306': 'CTRL',
+            '308': 'ALT',
+            '32': 'SPACE',
+            '282': 'F1',
+            '283': 'F2',
+            '284': 'F3',
+            '285': 'F4',
+            '286': 'F5',
+            '287': 'F6',
+            '288': 'F7',
+            '289': 'F8',
+            '290': 'F9',
+            '291': 'F10',
+            '292': 'F11',
+            '293': 'F12',
+            '305': 'CTRL',
+            '8': 'BACKSPACE',
+            '13': 'ENTER',
+            '303': 'SHIFT_R',
+            '273': 'ARROW_UP',
+            '274': 'ARROW_DOWN',
+            '275': 'ARROW_RIGHT',
+            '276': 'ARROW_LEFT'}
+
+control = {'ВВЕРХ': 'W',
+           'ВНИЗ': 'S',
            'ВЛЕВО': 'A',
            'ВПРАВО': 'D',
            'УДАР': 'Y',
            'СИЛЬНЫЙ УДАР': 'U',
            'УДАР НОГОЙ': 'I',
            'СИЛЬНЫЙ УДАР НОГОЙ': 'O',
-           'БЛОК': 'SPACE',
-           'X-RAY': 'P'}
+           'ПРЫЖОК': 'SPACE',
+           'X-RAY': 'T',
+           'ПАУЗА': 'P'}
 
 
 class Settings:
     def __init__(self, music_class, sound_class):
+        self.paragraph = 0
         self.points = [(30, 100, 'Музыка: вкл/выкл', (250, 250, 30), (250, 30, 250), 0),
                        (30, 200, 'Звуки: вкл/выкл', (250, 250, 30), (250, 30, 250), 1),
                        (30, 300, 'Назад', (250, 250, 30), (250, 30, 250), 2),
                        (450, 35, 'Управление', (250, 250, 30), (250, 30, 250), 3),
-                       (350, 100, 'ПРЫЖОК', (250, 250, 30), (250, 30, 250), 3),
-                       (350, 140, 'ПРИСЕСТЬ', (250, 250, 30), (250, 30, 250), 4),
+                       [350, 100, 'ВВЕРХ', (250, 250, 30), (250, 30, 250), 3],
+                       (350, 140, 'ВНИЗ', (250, 250, 30), (250, 30, 250), 4),
                        (350, 180, 'ВЛЕВО', (250, 250, 30), (250, 30, 250), 5),
                        (350, 220, 'ВПРАВО', (250, 250, 30), (250, 30, 250), 6),
                        (350, 260, 'УДАР', (250, 250, 30), (250, 30, 250), 7),
-                       (350, 300, 'СИЛ. УДАР', (250, 250, 30), (250, 30, 250), 8),
+                       (350, 300, 'СИЛ.УДАР', (250, 250, 30), (250, 30, 250), 8),
                        (350, 340, 'УДАР НОГОЙ', (250, 250, 30), (250, 30, 250), 9),
-                       (350, 380, 'СИЛ. УДАР НОГОЙ', (250, 250, 30), (250, 30, 250), 10),
-                       (350, 420, 'БЛОК', (250, 250, 30), (250, 30, 250), 11),
+                       (350, 380, 'СИЛ.УДАР НОГОЙ', (250, 250, 30), (250, 30, 250), 10),
+                       (350, 420, 'ПРЫЖОК', (250, 250, 30), (250, 30, 250), 11),
                        (350, 460, 'X-RAY', (250, 250, 30), (250, 30, 250), 12),
-                       (700, 100, control['ПРЫЖОК'], (250, 250, 30), (250, 30, 250), 3),
-                       (700, 140, control['ПРИСЕСТЬ'], (250, 250, 30), (250, 30, 250), 4),
-                       (700, 180, control['ВЛЕВО'], (250, 250, 30), (250, 30, 250), 5),
-                       (700, 220, control['ВПРАВО'], (250, 250, 30), (250, 30, 250), 6),
-                       (700, 260, control['УДАР'], (250, 250, 30), (250, 30, 250), 7),
-                       (700, 300, control['СИЛЬНЫЙ УДАР'], (250, 250, 30), (250, 30, 250), 8),
-                       (700, 340, control['УДАР НОГОЙ'], (250, 250, 30), (250, 30, 250), 9),
-                       (700, 380, control['СИЛЬНЫЙ УДАР НОГОЙ'], (250, 250, 30), (250, 30, 250), 10),
-                       (700, 420, control['БЛОК'], (250, 250, 30), (250, 30, 250), 11),
-                       (700, 460, control['X-RAY'], (250, 250, 30), (250, 30, 250), 12)]
+                       [620, 100, control['ВВЕРХ'], (250, 250, 30), (250, 30, 250), 3],
+                       [620, 140, control['ВНИЗ'], (250, 250, 30), (250, 30, 250), 4],
+                       [620, 180, control['ВЛЕВО'], (250, 250, 30), (250, 30, 250), 5],
+                       [620, 220, control['ВПРАВО'], (250, 250, 30), (250, 30, 250), 6],
+                       [620, 260, control['УДАР'], (250, 250, 30), (250, 30, 250), 7],
+                       [620, 300, control['СИЛЬНЫЙ УДАР'], (250, 250, 30), (250, 30, 250), 8],
+                       [620, 340, control['УДАР НОГОЙ'], (250, 250, 30), (250, 30, 250), 9],
+                       [620, 380, control['СИЛЬНЫЙ УДАР НОГОЙ'], (250, 250, 30), (250, 30, 250), 10],
+                       [620, 420, control['ПРЫЖОК'], (250, 250, 30), (250, 30, 250), 11],
+                       [620, 460, control['X-RAY'], (250, 250, 30), (250, 30, 250), 12]]
         self.music_class = music_class
         self.sound_class = sound_class
 
-    def clicked(self, paragraph):
-        if paragraph == 0:
+    def clicked(self, paragraph_number):
+        if paragraph_number == 0:
             if self.music_class.on:
                 self.music_class.not_play()
                 self.music_class.on = False
             else:
                 self.music_class.play()
                 self.music_class.on = True
-        elif paragraph == 1:
+        elif paragraph_number == 1:
             if self.sound_class.on:
                 self.sound_class.on = False
             else:
                 self.sound_class.on = True
-        elif paragraph == 2:
+        elif paragraph_number == 2:
             return 'main'
         # УПРАВЛЕНИЕ
-        elif paragraph == 3:
-            self.change('up_set')
-        elif paragraph == 4:
-            self.change('down_set')
-        elif paragraph == 5:
-            self.change('left_set')
-        elif paragraph == 6:
-            self.change('right_set')
-        elif paragraph == 7:
-            self.change('hit_set')
-        elif paragraph == 8:
-            self.change('power_hit_set')
-        elif paragraph == 9:
-            self.change('foot_set')
-        elif paragraph == 10:
-            self.change('power_foot_set')
-        elif paragraph == 11:
-            self.change('jump_set')
-        elif paragraph == 12:
-            self.change('xray_set')
+        elif paragraph_number == 3:
+            self.change(3)
+        elif paragraph_number == 4:
+            self.change(4)
+        elif paragraph_number == 5:
+            self.change(5)
+        elif paragraph_number == 6:
+            self.change(6)
+        elif paragraph_number == 7:
+            self.change(7)
+        elif paragraph_number == 8:
+            self.change(8)
+        elif paragraph_number == 9:
+            self.change(9)
+        elif paragraph_number == 10:
+            self.change(10)
+        elif paragraph_number == 11:
+            self.change(11)
+        elif paragraph_number == 12:
+            self.change(12)
 
     def change(self, set):
-        if set == 'up_set':
-            print(1)
-        elif set == 'down_set':
-            print('1')
-        elif set == 'left_set':
-            print('1')
-        elif set == 'right_set':
-            print('1')
-        elif set == 'hit_set':
-            print('1')
-        elif set == 'power_hit_set':
-            print('1')
-        elif set == 'foot_set':
-            print('1')
-        elif set == 'rower_foot_set':
-            print('1')
-        elif set == 'jump_set':
-            print('1')
-        elif set == 'xray_set':
-            print('1')
+        font = pygame.font.SysFont('serif', 30)
+        oops = True
+        screen.blit(font.render(self.points[set + 11][2], 2, (255, 36, 0)),
+                    (self.points[set + 11][0], self.points[set + 11][1]))
+        pygame.display.flip()
+        a = 0
+        while oops:
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    sys.exit()
+                elif i.type == pygame.KEYDOWN:
+                    key = i.key
+                    print(key)
+                    control[self.points[set + 1][2]] = str(key)
+                    for i in self.points:
+                        if i == self.points[set + 11]:
+                            if str(key) in keyboard:
+                                i[2] = keyboard[str(key)]
+                            else:
+                                i[2] = str(key)
+                    oops = False
 
     def drawing(self, surface, font_menu, number_point):
         for i in self.points:
             if i != self.points[3]:
+                # print(i[2])
                 if number_point == i[5]:
                     surface.blit(font_menu.render(i[2], 2, i[4]), (i[0], i[1]))
                 else:
@@ -232,30 +298,29 @@ class Settings:
 
     def run(self):
         font = pygame.font.SysFont('serif', 30)
-        paragraph = 0  # номер кнопки
         running = True
         while running:
             mouse = pygame.mouse.get_pos()
             for i in self.points:
                 # меняем подсвечиваемую кнопку, при наведении на неё мышкой
                 if mouse[0] > i[0] and mouse[0] < i[0] + 155 and mouse[1] > i[1] and mouse[1] < i[1] + 50:
-                    paragraph = i[5]
-            self.drawing(screen, font, paragraph)
+                    self.paragraph = i[5]
+            self.drawing(screen, font, self.paragraph)
             for i in pygame.event.get():
                 if i.type == pygame.QUIT:
                     sys.exit()
                 elif i.type == pygame.KEYDOWN:
                     if i.key == pygame.K_UP:
-                        paragraph = (paragraph - 1) % 13
+                        self.paragraph = (self.paragraph - 1) % 13
                     if i.key == pygame.K_DOWN:
-                        paragraph = (paragraph + 1) % 13
+                        self.paragraph = (self.paragraph + 1) % 13
                     # вызываем действия от подсвечиваемой кнопки при нажатии на enter
                     if i.key == 13:
-                        called_menu = self.clicked(paragraph)
+                        called_menu = self.clicked(self.paragraph)
                         running = False
                 # вызываем действия от подсвечиваемой кнопки при нажатии на левую кнопку мыши/enter
                 if i.type == pygame.MOUSEBUTTONDOWN and i.button == 1:
-                    called_menu = self.clicked(paragraph)
+                    called_menu = self.clicked(self.paragraph)
                     running = False
             screen.blit(screen, (0, 0))
             pygame.display.flip()
