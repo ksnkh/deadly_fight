c89797cd8ce66e50c8bb019cc4d47798f3c3bf6a
EgorMishalkin <cuperkotegor@yandex.ru>
2020-01-23 00:37:11 +0400

    first design and nothing works

--- connect_to_server.py
+++ connect_to_server.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 pygame.mixer.init()
@@ -8,21 +9,39 @@ screen = pygame.display.set_mode(size)
 
 class Connect:
     def __init__(self):
-        self.points = [[200, 35, 'Подключиться к серверу', (240, 240, 240), 3],
-                       [250, 100, 'Введите ip сервера', (128, 128, 128), 0],
-                       [175, 500, 'Назад', (128, 128, 128), 1],
-                       [400, 500, 'Подключиться к серверу', (128, 128, 128), 1]]
+        self.points = [[200, 35, 'Подключиться к серверу', (240, 240, 240)],
+                       [250, 100, 'Введите ip сервера', (228, 2, 11)],
+                       [175, 500, 'Назад', (228, 2, 11), 0],
+                       [400, 500, 'Подключиться к серверу', (228, 2, 11), 1]]
 
     def drawing(self, surface, font_menu):
-        screen.fill(pygame.Color('black'))
         for i in self.points:
             surface.blit(font_menu.render(i[2], 2, i[3]), (i[0], i[1]))
             surface.blit(font_menu.render(i[2], 2, i[3]), (i[0], i[1]))
             pygame.display.flip()
 
+    def clicked(self, paragraph_number):
+        if paragraph_number == 0:
+            self.change(3)
+        elif paragraph_number == 1:
+            self.change(4)
+
+    def change(self, set):
+        font = pygame.font.SysFont('mr_AfronikG', 38)
+        oops = True
+        pygame.display.flip()
+        a = 0
+        while oops:
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    sys.exit()
+
     def run(self):
         font = pygame.font.SysFont('mr_AfronikG', 38)
-        self.drawing(screen, font)
-        pygame.time.wait(2050)
+        running = True
+        while running:
+            self.drawing(screen, font)
+            screen.blit(screen, (0, 0))
+            pygame.display.flip()
         screen.fill(pygame.Color('black'))
         return 'main'
